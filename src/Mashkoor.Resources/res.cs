747b39ac12745588eac67764b0a8a3a9db32ed3a
Alaa Masoud <alaa.masoud@live.com>
2025-07-07 14:04:37 +0300

    Add initial packages

---
+++ src/Mashkoor.Resources/res.cs
@@ -0,0 +1,10 @@
+namespace Mashkoor.Resources;
+
+#pragma warning disable IDE1006 // Naming Styles
+#pragma warning disable CS8981 // The type name only contains lower-cased ascii characters. Such names may become reserved for the language.
+/// <summary>
+/// 
+/// </summary>
+public sealed class res { }
+#pragma warning restore CS8981 // The type name only contains lower-cased ascii characters. Such names may become reserved for the language.
+#pragma warning restore IDE1006 // Naming Styles
